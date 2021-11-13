c9e77d96bdb177298306ddf88494003f84ded27a
Johannes Link <business@johanneslink.net>
2021-11-13 12:49:31 +0100

    Upgrade to jqwik 1.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,8 @@ repositories {
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-    testImplementation("net.jqwik:jqwik:1.6.0-SNAPSHOT")
-    testImplementation("net.jqwik:jqwik-kotlin:1.6.0-SNAPSHOT")
+    testImplementation("net.jqwik:jqwik:1.6.0")
+    testImplementation("net.jqwik:jqwik-kotlin:1.6.0")
     testImplementation("org.assertj:assertj-core:3.21.0")
     testImplementation("io.kotest:kotest-runner-junit5:4.6.3")
     testImplementation("io.kotest:kotest-property-jvm:4.6.3")
