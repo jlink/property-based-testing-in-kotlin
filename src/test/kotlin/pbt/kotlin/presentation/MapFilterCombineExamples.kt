20eb443ce8540338dd8133e6cf3668564ffbd779
Johannes Link <business@johanneslink.net>
2022-01-17 15:10:04 +0100

    Adapted combine example

--- src/test/kotlin/pbt/kotlin/presentation/MapFilterCombineExamples.kt
+++ src/test/kotlin/pbt/kotlin/presentation/MapFilterCombineExamples.kt
@@ -1,9 +1,14 @@
 package pbt.kotlin.presentation
 
 import net.jqwik.api.*
+import net.jqwik.api.Arbitraries.just
+import net.jqwik.api.constraints.CharRange
+import net.jqwik.api.constraints.LowerChars
+import net.jqwik.api.constraints.StringLength
 import net.jqwik.kotlin.api.any
 import net.jqwik.kotlin.api.combine
 import net.jqwik.kotlin.api.ofLength
+import net.jqwik.kotlin.api.orNull
 import java.util.*
 
 
@@ -33,9 +38,10 @@ class MapFilterCombineExamples {
             .map(Integer::toHexString)
 
     @Property(tries = 50)
-    @Report(Reporting.GENERATED)
-    fun anyValidPersonHasAFullName(@ForAll("valid people") aPerson: Person): Boolean {
-        return aPerson.fullName().length >= 5
+    //@Report(Reporting.GENERATED)
+    fun anyValidPersonHasAFullName(@ForAll("valid people 2") aPerson: Person): Boolean {
+        println(aPerson.fullName())
+        return aPerson.fullName().length >= 2
     }
 
     @Provide
@@ -46,6 +52,14 @@ class MapFilterCombineExamples {
         val lastName = String.any().withCharRange('a', 'z')
             .ofLength(2..20)
         return combine(firstName, lastName) { first, last -> Person(first, last) }
+    }
+
+    @Provide
+    fun `valid people 2`(
+        @ForAll firstName: @LowerChars @StringLength(min = 2, max = 10) String,
+        @ForAll lastName: @LowerChars @StringLength(min = 2, max = 20) String
+    ): Arbitrary<Person> {
+        return just(Person(firstName.capitalize(), lastName))
     }
 
     private fun String.capitalize() = this.substring(0, 1).uppercase(Locale.getDefault()) + this.substring(1)
