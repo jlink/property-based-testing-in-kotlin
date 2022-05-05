513747d21f46ebfdba4367dfafa3aae538aae94a
Johannes Link <business@johanneslink.net>
2022-05-05 13:15:15 +0200

    Added kotest shrinking example

--- src/test/kotlin/pbt/kotlin/kotest/KotestExamples.kt
+++ src/test/kotlin/pbt/kotlin/kotest/KotestExamples.kt
@@ -5,9 +5,20 @@ import io.kotest.property.Arb
 import io.kotest.property.arbitrary.int
 import io.kotest.property.arbitrary.list
 import io.kotest.property.checkAll
+import io.kotest.property.forAll
 import org.assertj.core.api.Assertions.assertThat
 
 class KotestExamples : StringSpec({
+
+
+    "shrink int values" {
+        forAll(Arb.int(1000..1_000_000)) { anInt ->
+            // Should shrink to 90000 but does not most of the time
+            // anInt < 90000
+            true
+        }
+    }
+
     "reversing keeps all elements" {
         checkAll<List<Int>> { list ->
             assertThat(list.reversed()).containsAll(list)
