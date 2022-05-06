2cc30dc949d48a90f6a1d17423fc534de3829627
Johannes Link <business@johanneslink.net>
2022-05-06 10:51:35 +0200

    More minor changes for demo

--- src/test/kotlin/pbt/kotlin/jqwik/KotlinFunctionExamples.kt
+++ src/test/kotlin/pbt/kotlin/jqwik/KotlinFunctionExamples.kt
@@ -2,9 +2,10 @@ package pbt.kotlin.jqwik
 
 import net.jqwik.api.ForAll
 import net.jqwik.api.Property
+import net.jqwik.api.constraints.Size
 import org.assertj.core.api.Assertions.assertThat
 
-class KotlinFunctionProperties {
+class KotlinFunctionExamples {
 
     @Property(tries = 10)
     fun `all generated functions return a String`(
@@ -13,7 +14,8 @@ class KotlinFunctionProperties {
         @ForAll int2: Int
     ) {
         assertThat(func(int1, int2)).isInstanceOf(String::class.java)
-        println(func(int1, int2))
-        println(func(int1, int2))
+        println("f($int1, $int2) = ${func(int1, int2)}")
+        println("f($int1, $int2) = ${func(int1, int2)}")
+        println("f($int2, $int1) = ${func(int2, int1)}")
     }
 }
